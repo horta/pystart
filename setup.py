22eaa43e49ecab78568de0a23cf0f815d46f8596
Danilo Horta <danilo.horta@gmail.com>
2018-01-09 22:51:25 +0000

    pypandoc

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md').read()
+
 if __name__ == '__main__':
     console_scripts = ["pystart = pystart.cmd:entry_point"]
-    setup(entry_points=dict(console_scripts=console_scripts))
+    setup(
+        entry_points=dict(console_scripts=console_scripts),
+        long_description=long_description)
