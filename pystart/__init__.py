2ae2ff822414c075e7baf2d398fd14d1622fd0d9
Danilo Horta <danilo.horta@gmail.com>
2018-01-09 23:28:38 +0000

    install_requires

--- pystart/__init__.py
+++ pystart/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import as _
 from . import (config, licenses, classifiers, changelog, manifest, username,
                progress)
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 __all__ = [
     "__version__", "config", "licenses", "classifiers", "changelog",
