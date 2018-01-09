f4d6a8f30e9b215e6f39f63bff17f87bc69cb70d
Danilo Horta <danilo.horta@gmail.com>
2018-01-09 23:02:12 +0000

    pypandoc convert_text

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
+import pypandoc
 from setuptools import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    long_description = open('README.md').read()
-
 if __name__ == '__main__':
+    readme = open('README.md').read()
+    long_description = pypandoc.convert_text(
+        readme, 'rst', format='markdown_github')
+
     console_scripts = ["pystart = pystart.cmd:entry_point"]
     setup(
         entry_points=dict(console_scripts=console_scripts),
