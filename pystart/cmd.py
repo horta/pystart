558dd559a021c3e5edf53c94b7af9628b8c06741
Danilo Horta <danilo.horta@gmail.com>
2018-01-09 14:34:23 +0000

    create folders and files

--- pystart/cmd.py
+++ pystart/cmd.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import as _
 
+import os
 from inquirer import prompt, Text, List, Checkbox
 from .config import get_config_file
 from .licenses import LICENSES
@@ -7,6 +8,19 @@ from .classifiers import PLATFORMS
 from .config import get_config_file
 from validate_email import validate_email
 from .pip_search import pip_exact_search
+
+SETUP_PY_CONTENT = r"""from setuptools import setup
+
+if __name__ == '__main__':
+    setup()
+"""
+
+INIT_CONTENT = r"""from __future__ import absolute_import as _
+
+__version__ = "{VERSION}"
+
+__all__ = ["__version__"]
+"""
 
 
 class InvalidConfigError(Exception):
@@ -16,6 +30,7 @@ class InvalidConfigError(Exception):
 class Project(object):
     def __init__(self):
         self._metadata = dict()
+        self._cfg = ""
 
     def ask_questions(self):
         # questions = [
@@ -27,6 +42,7 @@ class Project(object):
         #         validate=lambda _, x: validate_email(x)),
         #     Text('description', message="Brief description the package"),
         #     Text('keywords', message="Comma-delimited list of Keywords"),
+        #     Text('version', message="What's the initial version"),
         #     List(
         #         'license',
         #         message="What's the package license",
@@ -39,8 +55,9 @@ class Project(object):
         # ]
         # answers = prompt(questions)
         answers = dict(
-            name="pystart",
+            name="pystart2",
             author="Danilo Horta",
+            version="0.1.0",
             author_email="danilo.horta@gmail.com",
             description="Breif description",
             license="MIT",
@@ -56,7 +73,7 @@ class Project(object):
         url = 'https://github.com/project/package'
         self._metadata['url'] = url
 
-    def check_package_name_availability(self):
+    def check_pkgname(self):
         name = self._metadata['name']
         found = pip_exact_search(name)
         if found:
@@ -75,7 +92,34 @@ class Project(object):
         cfg = cfg.replace("{KEYWORDS}", ', '.join(self._metadata["keywords"]))
         cfg = cfg.replace("{PLATFORMS}",
                           ', '.join(self._metadata["platforms"]))
-        print(cfg)
+        self._cfg = cfg
+
+    def _create_folders(self):
+        name = self._metadata['name']
+        os.mkdir(name)
+        os.mkdir(os.path.join(name, name))
+
+    def _create_init_file(self):
+        name = self._metadata['name']
+        version = self._metadata['version']
+
+        with open(os.path.join(name, name, '__init__.py'), 'w') as f:
+            content = INIT_CONTENT.replace("{VERSION}", version)
+            f.write(content)
+
+    def _create_setup_files(self):
+        name = self._metadata['name']
+
+        with open(os.path.join(name, 'setup.py'), 'w') as f:
+            f.write(SETUP_PY_CONTENT)
+
+        with open(os.path.join(name, 'setup.cfg'), 'w') as f:
+            f.write(self._cfg)
+
+    def create_project(self):
+        self._create_folders()
+        self._create_init_file()
+        self._create_setup_files()
 
 
 def entry_point():
@@ -84,7 +128,8 @@ def entry_point():
     try:
         p.ask_questions()
         p.fill_default()
-        p.check_package_name_availability()
+        p.check_pkgname()
         p.fill_cfg()
+        p.create_project()
     except InvalidConfigError:
         pass
--- pystart/config.py
+++ pystart/config.py
@@ -2,4 +2,4 @@ def get_config_file():
     import os
     path = os.path.abspath(__file__)
     f = os.path.join(os.path.dirname(path), 'setup.cfg.tpl')
-    return open(f).read().strip()
+    return open(f).read()
