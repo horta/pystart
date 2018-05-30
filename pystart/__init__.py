f60285972bda958819d6a1408325ed0a852ebcf8
Danilo Horta <danilo.horta@gmail.com>
2018-05-30 16:07:11 +0100

    0.1.12

--- pystart/__init__.py
+++ pystart/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import as _
 from . import (config, licenses, classifiers, changelog, manifest, username,
                progress)
 
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 __all__ = [
     "__version__", "config", "licenses", "classifiers", "changelog",
--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    readme = open('README.md').read()
-    try:
-        import pypandoc
-        long_description = pypandoc.convert_text(
-            readme, 'rst', format='markdown_github')
-    except (ImportError, RuntimeError):
-        long_description = readme
-
     console_scripts = ["pystart = pystart.cmd:entry_point"]
-    setup(
-        entry_points=dict(console_scripts=console_scripts),
-        long_description=long_description)
+    setup(entry_points=dict(console_scripts=console_scripts))
